910ad37dbe90f0a7dae8ef4ddfab0135642129be
Mateusz <mateusz.ostro@o2.pl>
2023-06-28 19:22:04 +0200

    Imported new classes

--- src/main/java/org/example/controllers/HomeController.java
+++ src/main/java/org/example/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package org.example.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("Spring")
+    private String topicName;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello 2";
+    }
+
     @GetMapping("/ok")
     public String getHomeMapping() {
         return "Hello!";
+    }
+
+    @GetMapping("/topic")
+    public String getTopic() {
+        return "Topic " + topicName;
     }
 
 }
